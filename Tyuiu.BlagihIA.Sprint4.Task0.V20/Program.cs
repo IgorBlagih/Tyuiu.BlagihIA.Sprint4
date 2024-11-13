99dcd777f6f3194660d0483261cdb10bb5cd39de
Игорь Благих <Tyuiu.Blagihia@outlook.com>
2024-11-13 20:33:35 +0500

    Task0

--- Tyuiu.BlagihIA.Sprint4.Task0.V20/Program.cs
+++ Tyuiu.BlagihIA.Sprint4.Task0.V20/Program.cs
@@ -8,18 +8,19 @@ namespace Tyuiu.BlagihIA.Sprint4.Task0.V20
             DataService ds = new DataService();
 
             Console.WriteLine("***************************************************************************");
-            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Спринт #4                                                               *");
             Console.WriteLine("* Тема: Базовые навыки  работы в С#                                       *");
             Console.WriteLine("* Задание #0                                                              *");
-            Console.WriteLine("* Вариант #28                                                              *");
+            Console.WriteLine("* Вариант 20                                                              *");
             Console.WriteLine("* Выполнил: Благих Игорь Александрович | ИИПб-24-2                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                                *");
-            Console.WriteLine("* Написать программу используя цикл for, которая вычисляет произведение   *");
-            Console.WriteLine("* ряда по формуле, при X=0,25                                             *");
+            Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов заполненный         *");
+            Console.WriteLine("* заполненный  значениями  в диапазоне от 0 до 9 подсчитать               *");
+            Console.WriteLine("* произведение четных элементов массива.                                  *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
-            Console.WriteLine("* x = 0.25                                                                *");
+            Console.WriteLine("* {4 ,8 ,7 ,6 ,5 ,8 ,2 ,4 ,3 ,2}                                          *");
             Console.WriteLine("***************************************************************************");
 
             int[] array = new int[] { 4, 8, 7, 6, 5, 8, 2, 4, 3, 2 }; 
